dee0ea60d9f91a03722bb1a3946020799016c485
jeongnaehyeok <cnh0301@gmail.com>
2020-12-08 11:46:09 +0900

    [ Feature ] Admin 추가

--- app/admin.py
+++ app/admin.py
@@ -1,1 +1,12 @@
 from django.contrib import admin
+from .models import NPoems
+
+
+class NPoemsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nickname', 'level', 'created_at']
+    list_filter = ['word', 'level']
+    list_per_page = 10
+    search_fields = ['nickname']
+
+
+admin.site.register(NPoems, NPoemsAdmin)
